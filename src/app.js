31dbc1ea101a6a54b8a8e4adac1dc16c5a9d92e3
Roderick Shoolbraid <rdxenv@gmail.com>
2024-11-29 22:50:20 -0800

    project port and name change

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // Example API endpoint
 app.get('/api/data', (req, res) => {
-    res.json({ message: 'Welcome to Project E API!' });
+    res.json({ message: 'Welcome to Project F API!' });
 });
 
 // Serve the main HTML file
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
-    console.log(`Project E is running on http://localhost:${PORT}`);
+    console.log(`Project F is running on http://localhost:${PORT}`);
 });
