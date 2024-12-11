51bb9663992eb5948042856b3753a734f113bc0e
Roderick Shoolbraid <rdxenv@gmail.com>
2024-12-11 10:27:27 -0800

    const file for API

---
+++ config/const.js
@@ -0,0 +1,7 @@
+// const.js 
+module.exports = {
+  PROJECT_F_URL: 'http://localhost:3006/api/messages',
+  PROJECT_B_URL: 'http://localhost:3002',
+  PROJECT_A_URL: 'http://localhost:3001/api/receive-jobs',
+  // Add other URLs or constants as needed
+};
