d46a1bfadcf9cd8346467e08158a3c6aabb5f272
Roderick Shoolbraid <rdxenv@gmail.com>
2024-12-14 17:37:33 -0800

    added routes URLS

--- config/const.js
+++ config/const.js
@@ -1,7 +1,7 @@
 // const.js 
 module.exports = {
-  PROJECT_F_URL: 'http://localhost:3006/api/messages',
+  PROJECT_F_NOTIFICATIONS_URL: 'http://localhost:3006/api/notifications',
+  PROJECT_F_API_MESSAGES_URL: 'http://localhost:3006/api/messages',
   PROJECT_B_URL: 'http://localhost:3002',
   PROJECT_A_URL: 'http://localhost:3001/api/receive-jobs',
-  // Add other URLs or constants as needed
 };
